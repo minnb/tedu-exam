79ead8c00b94f17b8698f259106568a370d2031e
minhnb <minhnb.it@gmail.com>
2021-08-17 22:31:31 +0700

    first

---
+++ src/Identity/Identity.API/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API
+{
+    public class AppSettings
+    {
+        public string ExamWebAppClient { set; get; }
+        public string ExamWebAdminClient { set; get; }
+        public string ExamWebApiClient { set; get; }
+    }
+}
